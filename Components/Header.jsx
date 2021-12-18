410c3ea419801d70bad51efaa39462ec65d929f2
Md Raisul Islam <mdraisulislam378@gmail.com>
2021-12-17 22:50:08 -0800

    Tagapp first commit

---
+++ Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-web'
+
+const Header = () => {
+    return (
+        <SafeAreaView>
+            <Text>It's Header</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Header
---
+++ Pages/Home/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const HomeLayout = () => {
+    return (
+        <View>
+            <Text>Its Home Layout</Text>
+        </View>
+    )
+}
+
+export default HomeLayout
